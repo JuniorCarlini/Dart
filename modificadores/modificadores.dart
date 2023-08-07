6c0ec61fc179e6d51d69149eb18ecf1314b4eaa1
Junior Carlini <116855041+JuniorCarlini@users.noreply.github.com>
2023-08-07 13:54:39 -0300

    modificadores

---
+++ modificadores/modificadores.dart
@@ -0,0 +1,13 @@
+void main() {
+  //final (depois de criada não pode ser alterada)
+  //ultilizar sem moderação
+  final nomeCompleto = "Junior Carlini";
+
+  //const (depois de criada não pode ser alterada, mas ela deve ser definida em momento de compilação )
+  //não podem receber valores de outras variaveis a não ser que essas variaveis forem const tembem
+  const nomeCompletoConst = "Junior Carlini";
+
+
+  print(nomeCompleto);
+  print(nomeCompletoConst);
+}
