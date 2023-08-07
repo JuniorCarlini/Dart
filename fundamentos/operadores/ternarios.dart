fdf25092ff58188d504a80d3d72ea376bdef5619
Junior Carlini <116855041+JuniorCarlini@users.noreply.github.com>
2023-08-07 14:59:00 -0300

    estudo sobre ternarios

---
+++ fundamentos/operadores/ternarios.dart
@@ -0,0 +1,9 @@
+void main() {
+  //ternarios
+  //complexo não é recomendado usar
+  final idade = 12;
+
+  //(condição) ? faça algo : outra coisa
+  final eMaiorDeIdade = (idade >= 18 ? true : false);
+  print("é maior de idade?" + eMaiorDeIdade.toString());
+}
