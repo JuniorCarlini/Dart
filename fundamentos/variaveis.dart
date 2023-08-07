eea32846be4a194f74cd03d80901001a4fef74f6
Junior Carlini <116855041+JuniorCarlini@users.noreply.github.com>
2023-08-07 13:32:41 -0300

    ajuste

--- fundamentos/variaveis.dart
+++ fundamentos/variaveis.dart
@@ -1,29 +1,29 @@
 void main() {
-    //o tipo num é o pai do int e do double
-    num numeroAleatorio = 234;
+  //o tipo num é o pai do int e do double
+  num numeroAleatorio = 234;
 
-    //numeros inteiros 
-    int idade = 37;
+  //numeros inteiros 
+  int idade = 37;
 
-    //numeros quebrados
-    double valorIphone = 12.000;
+  //numeros quebrados
+  double valorIphone = 12.000;
 
-    //tipo padrao para nomear variaveis no dart (camelCase)
-    //ex: valorDoIphone
-    //existem outros tipos como o SnackCase
-    //ex: valor_do_iphone
+  //tipo padrao para nomear variaveis no dart (camelCase)
+  //ex: valorDoIphone
+  //existem outros tipos como o SnackCase
+  //ex: valor_do_iphone
 
-    String nomeCursoFlutter = "teste";
+  String nomeCursoFlutter = "teste";
 
-    //tipos dinamicos
-    Object objetoQualquer = 1.2;
-    dynamic qualquerTipoDeValor = 1;
+  //tipos dinamicos
+  Object objetoQualquer = 1.2;
+  dynamic qualquerTipoDeValor = 1;
 
-    print(idade);
-    print(valorIphone);
-    print(numeroAleatorio);
-    print(nomeCursoFlutter);
-    print(objetoQualquer);
-    print(qualquerTipoDeValor);
+  print(idade);
+  print(valorIphone);
+  print(numeroAleatorio);
+  print(nomeCursoFlutter);
+  print(objetoQualquer);
+  print(qualquerTipoDeValor);
 
 }
