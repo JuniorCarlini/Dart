141818c33d1eced959bce9506e9a1f842064cbc9
Junior Carlini <116855041+JuniorCarlini@users.noreply.github.com>
2023-08-07 18:37:23 -0300

    estudos while do while

---
+++ fundamentos/loops/while_do_while.dart
@@ -0,0 +1,22 @@
+void main(){
+
+  var numero = 0;
+
+  //while convercional 
+  while(numero <= 10){
+    print(numero);
+    numero++;
+  }
+
+  //doWhile 
+  var indice = 0;
+  //o while não sera executado
+  while(indice > 0){
+    print(indice);
+    indice++;
+  }do{
+    print(indice);
+      indice++;
+    }while(indice > 0);
+  
+}
