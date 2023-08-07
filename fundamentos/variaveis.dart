34006b9998ac17478c43d27a573b75087d165e93
Junior Carlini <116855041+JuniorCarlini@users.noreply.github.com>
2023-08-07 13:12:56 -0300

    estudo de variaveis

---
+++ fundamentos/variaveis.dart
@@ -0,0 +1,29 @@
+void main() {
+    //o tipo num é o pai do int e do double
+    num numeroAleatorio = 234;
+
+    //numeros inteiros 
+    int idade = 37;
+
+    //numeros quebrados
+    double valorIphone = 12.000;
+
+    //tipo padrao para nomear variaveis no dart (camelCase)
+    //ex: valorDoIphone
+    //existem outros tipos como o SnackCase
+    //ex: valor_do_iphone
+
+    String nomeCursoFlutter = "teste";
+
+    //tipos dinamicos
+    Object objetoQualquer = 1.2;
+    dynamic qualquerTipoDeValor = 1;
+
+    print(idade);
+    print(valorIphone);
+    print(numeroAleatorio);
+    print(nomeCursoFlutter);
+    print(objetoQualquer);
+    print(qualquerTipoDeValor);
+
+}
