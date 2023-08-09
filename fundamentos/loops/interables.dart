9bf574f9678813ccc78585971f11e32373a66157
Junior Carlini <116855041+JuniorCarlini@users.noreply.github.com>
2023-08-09 10:58:32 -0300

    estudo sobre interables

--- fundamentos/loops/interables.dart
+++ fundamentos/loops/interables.dart
@@ -4,7 +4,13 @@ void main() {
 
   numeros
     .where((numero) => numero != 5)
-    .forEach((numero) => print(numero)); //só deve ser usado se nao estiver usando nhnhuma funçao assincrona
-  final numerosAte5 = numeros.takeWhile((numero) => numero < 6);
+    .forEach((numero) => print(numero)); //só deve ser usado se nao estiver usando nenhuma funçao assincrona
+  
+  final numerosAte5 = numeros
+  .takeWhile((numero) => numero < 6)
+  .where((numero) => numero != 5)
+  .toList();
+
   print(numerosAte5);
+
 }
